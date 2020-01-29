0ce98d4c6fd109d0b07ff1baabbe5980387d95b1
Nicolas Hoffmann <dante3333@gmail.com>
2020-01-29 17:49:11 +0100

    Fix - moved styles to Design system & label display

--- src/app/components/list/ItemLabels.tsx
+++ src/app/components/list/ItemLabels.tsx
@@ -5,12 +5,11 @@ import { noop } from 'proton-shared/lib/helpers/function';
 import { Link } from 'react-router-dom';
 import { Icon, classnames } from 'react-components';
 
+import { c } from 'ttag';
 import { Label } from '../../models/label';
 import { Element } from '../../models/element';
 import { getLabelIDs } from '../../helpers/elements';
 import { getLabelsWithoutFolders } from '../../helpers/labels';
-
-import './ItemLabel.scss';
 
 interface Props {
     element?: Element;
@@ -28,20 +27,37 @@ const ItemLabels = ({ element = {}, onUnlabel = noop, max = 99, labels = [], cla
     const labelsToShow = labelsSorted.slice(0, max);
 
     return (
-        <div className={classnames(['pm_labels stop-propagation', className])}>
+        <div
+            className={classnames([
+                'inline-flex flew-row flex-items-center pm-badgeLabel-container stop-propagation',
+                className
+            ])}
+        >
             {labelsToShow.map(({ ID = '', Name = '', Color = '' }) => (
                 <span
-                    className="pm_label"
+                    className="badgeLabel flex flex-row flex-items-center ml0-25"
                     style={{
                         backgroundColor: Color,
                         borderColor: Color
                     }}
                     key={ID}
                 >
-                    <Link to={`/${ID}`}>{Name}</Link>
+                    <Link
+                        to={`/${ID}`}
+                        className="pm-badgeLabel-link color-white nodecoration"
+                        title={c('Action').t`Display emails labelled with ${Name}`}
+                    >
+                        {Name}
+                    </Link>
                     {onUnlabel !== noop ? (
-                        <button type="button" onClick={() => onUnlabel(ID)}>
+                        <button
+                            type="button"
+                            className="flex pm-badgeLabel-button flex-item-noshrink"
+                            onClick={() => onUnlabel(ID)}
+                            title={c('Action').t`Remove this label`}
+                        >
                             <Icon name="off" size={12} color="white" />
+                            <span className="sr-only">{c('Action').t`Remove this label`}</span>
                         </button>
                     ) : null}
                 </span>
